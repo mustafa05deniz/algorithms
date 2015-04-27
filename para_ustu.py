826c23efb02f49600cda280e8727167f2f5617fd
Mustafa <mustafa05deniz@gmail.com>
2015-04-27 09:55:50 +0300

    Update para_ustu.py

--- para_ustu.py
+++ para_ustu.py
@@ -1,3 +1,10 @@
+
+# Para üstü problemi Verilen bir Paradan masrafı çıkardığında geriye kalan para üstünün hangi banknotlar halinde verileceğini 
+# çözen bir algoritma 
+
+# Kriterler : 
+    # Para birimi listesinin büyükten küçüğe doğru harekete ettigini varsayıyoruz
+
 def Para(verilen_para,masraf):
     birim=[17,15,1]
     para_ustu=verilen_para-masraf
@@ -8,8 +15,4 @@ def Para(verilen_para,masraf):
         bolunen_liste.append(a)
         return bolunen_liste
         
-        
-    
-    
-
 print(Para(100,55))
